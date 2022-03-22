511df5466fa25954f4a1e53d1751fd7e557aa582
anyuna <anyuna00@naver.com>
2022-03-22 23:27:48 +0900

    3주차 실습 조건문 예제들 짝수 홀수 판별 숫자->한글 숫자 크기 비교 월의 일수

---
+++ 3주차 실습/ch.3/src/DaylnMonth.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class DaylnMonth {
+
+	public static void main(String[] args) {
+		int month;
+		int days = 0;
+		
+		System.out.print("일수를 알고 싶은 월을 입력하시오.");
+		Scanner scan = new Scanner(System.in);
+		
+		month = scan.nextInt();
+		switch(month) {
+		case 2:
+			days = 28;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			days = 30;
+			break;
+		
+		default:
+			days = 31;
+			break;
+		}
+		System.out.println(month+"월의 일수는" + days);
+
+	}
+
+}
